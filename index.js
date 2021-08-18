32bc4a0cd90b7d50201dc65a99b901c8f5ff2215
Ilya Spivakov <ilya@spivakov.me>
2021-08-17 22:01:49 -0700

    first working version

--- index.js
+++ index.js
@@ -1,13 +1,59 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
+const generateMarkdown = require('./utils/generateMarkdown');
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Questions for user input
+const questions = [
+    {name: 'fileName', type: 'input', message: 'Enter filename', default: 'README-TEST.md'},
+    {name: 'title', type: 'input', message: 'Project Title?'},
+    {name: 'description', type: 'input', message: 'Description?'},
+    {name: 'installation', type: 'editor', message: 'Installation instructions'},
+    {name: 'usage', type: 'editor', message: 'Usage?'},
+    {name: 'license', type: 'list', message: 'Choose License', choices:[
+        {name: 'GNU AGPLv3', value: 'agpl-3.0'},
+        {name: 'GNU GPLv3', value: 'gpl-3.0'},
+        {name: 'GNU LGPLv3', value: 'lgpl-3.0'},
+        {name: 'Mozilla Public License 2.0', value: 'mpl-2.0'},
+        {name: 'Apache License 2.0', value: 'apache-2.0'},
+        {name: 'MIT License', value: 'mit'},
+        {name: 'Boost Software License 1.0', value: 'bsl-1.0'},
+        {name: 'The Unlicense', value: 'unlicense'},]},
+    {name: 'contributing', type: 'editor', message: 'How to contribute?',
+        default: 
+`Contributions are what make the open source community such an amazing place to be learn, inspire, and create. Any contributions you make are **greatly appreciated**.
 
-// TODO: Create a function to initialize app
-function init() {}
+  1. Fork the Project
+  2. Create your Feature Branch (\`git checkout -b feature/AmazingFeature\`)
+  3. Commit your Changes (\`git commit -m 'Add some AmazingFeature'\`)
+  4. Push to the Branch (\`git push origin feature/AmazingFeature\`)
+  5. Open a Pull Request
+`},
+    {name: 'tests', type: 'input', message: 'Tests?'},
+    {name: 'email', type: 'input', message: 'email?'},
+    {name: 'github', type: 'input', message: 'GitHub username?'},
+];
+
+// A function to write README file
+function writeToFile(fileName, data) {
+    console.log(data);
+    const text = generateMarkdown(data);
+
+    fs.writeFile(fileName, text, function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+      });
+
+}
+
+// A function to initialize app
+function init() {
+    inquirer
+    .prompt(questions)
+    .then(answers => writeToFile(answers.fileName, answers));
+
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,109 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const licenseLookup = {
+    'agpl-3.0': {
+        badge: 'https://img.shields.io/badge/License-AGPL%20v3-blue.svg',
+        description: 'GNU AGPLv3',
+        url: '(http://www.gnu.org/licenses/agpl-3.0)'},
+    'gpl-3.0': {
+        badge: 'https://img.shields.io/badge/License-GPL%20v3-blue.svg',
+        description: 'GNU GPLv3',
+        url: 'http://www.gnu.org/licenses/gpl-3.0'},
+    'lgpl-3.0': {
+        badge: 'https://img.shields.io/badge/License-LGPL%20v3-blue.svg',
+        description: 'GNU LGPLv3',
+        url: 'http://www.gnu.org/licenses/lgpl-3.0'},
+    'mpl-2.0': {
+        badge: 'https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg',
+        description: 'Mozilla Public License 2.0',
+        url: 'https://opensource.org/licenses/MPL-2.0'},
+    'apache-2.0': {
+        badge: 'https://img.shields.io/badge/License-Apache%202.0-blue.svg',
+        description: 'Apache License 2.0',
+        url: 'https://opensource.org/licenses/Apache-2.0'},
+    'mit': {
+        badge: 'https://img.shields.io/badge/License-MIT-yellow.svg',
+        description: 'MIT License',
+        url: 'https://opensource.org/licenses/MIT'},
+    'bsl-1.0': {
+        badge: 'https://img.shields.io/badge/License-Boost-yellow.svg',
+        description: 'Boost Software License 1.0',
+        url: 'https://opensource.org/licenses/BSL-1.0'},
+    'unlicense': {
+        badge: 'https://img.shields.io/badge/License-Unlicense-green.svg',
+        description: 'The Unlicense',
+        url: 'https://opensource.org/licenses/unlicense'},
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
+// A function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseBadge(license) {
+    if(license in licenseLookup){
+        const l = licenseLookup[license];
+        return `[![${l.description}](${l.badge})](${l.url})`;
+    }else{
+        return '';
+    }
+}
 
-// TODO: Create a function to generate markdown for README
+// A function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+    if(license in licenseLookup){
+        const l = licenseLookup[license];
+        return `[${l.description}](${l.url})`;
+    }else{
+        return '';
+    }
+}
+
+// A function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+    if(license in licenseLookup){
+        const l = licenseLookup[license];
+        return `Licensed under ${renderLicenseLink(license)}`;
+    }else{
+        return '';
+    }
+}
+
+/**
+ * Generate markdown text from data
+ * @param {*} data 
+ * @returns 
+ */
 function generateMarkdown(data) {
   return `# ${data.title}
+${renderLicenseBadge(data.license)}
 
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+${renderLicenseSection(data.license)}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+For questions contact [${data.email}](mailto:${data.email}) or [github](https://github.com/${data.github}).
 `;
 }
 
